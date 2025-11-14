f4796f1245a5941aaf52b609f8861564a8fba19e
Vedant Nipane <98020338+VedantNipane@users.noreply.github.com>
2025-11-14 20:34:05 +0530

    Add test function to call main

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,12 @@
 # hello_world.py
 
+def test():
+    main()
+
 def main():
     print("Hello, World!")       # English
     print("¡Hola, Mundo!")       # Spanish
 
 if __name__ == "__main__":
     main()
+    test()
